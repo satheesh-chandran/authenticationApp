4328eae2206dadaf91e5ae70a873a61931a1364e
Satheesh C <satheesancchandran@gmail.com>
2020-09-12 17:24:53 +0530

    Added env configurations

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+require('dotenv').config({ path: './.env' });
+const { env } = process;
+const { DbClient, DatabaseUrlT, DatabaseUrl } = env;
+
+module.exports = {
+  development: {
+    client: DbClient,
+    connection: {
+      filename: DatabaseUrl
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: DbClient,
+    connection: {
+      filename: DatabaseUrlT
+    },
+    useNullAsDefault: true
+  }
+};
