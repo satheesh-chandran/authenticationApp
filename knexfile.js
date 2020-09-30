557eb82c152d9e940d06328764d3a2c86ad9caff
Satheesh C <satheesancchandran@gmail.com>
2020-09-30 10:42:29 +0530

    Removed unnecessary scripts in package.json

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,11 @@
 require('dotenv').config({ path: './.env' });
 const { env } = process;
-const { DbClient, DatabaseUrlT, DatabaseUrl, PG_database, DATABASE_URL } = env;
-
-// module.exports = {
-//   development: {
-//     client: DbClient,
-//     connection: {
-//       filename: DatabaseUrl
-//     },
-//     useNullAsDefault: true
-//   },
-
-//   test: {
-//     client: DbClient,
-//     connection: {
-//       filename: DatabaseUrlT
-//     },
-//     useNullAsDefault: true
-//   }
-// };
+const { DbClient, DATABASE_URL } = env;
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: env.DATABASE_URL,
+    client: DbClient,
+    connection: DATABASE_URL,
     seeds: { directory: `${__dirname}/seeds` },
     migrations: { directory: `${__dirname}/migrations` }
   }
